6be149e1f023d4573c0240963d50279791fc86f3
Matt Anderson <lucid@wakeupdreamer.com.au>
2019-09-17 17:54:36 +0930

    Relcoated instructions to Readme.

--- smtp_mail.php
+++ smtp_mail.php
@@ -11,16 +11,6 @@ License:      MIT License
 
 add_action('phpmailer_init', 'SMTPMail::mail');
 
-/*
-Define the following constants in functions.php or config/application.php
-  define('USE_SMTP', true);
-  define('SMTP_HOST', env('SMTP_HOST'));
-  define('SMTP_USER', env('SMTP_USER'));
-  define('SMTP_PASSWORD', env('SMTP_PASSWORD'));
-  define('SMTP_PORT', env('SMTP_PORT'));
-  define('SMTP_FROM_EMAIL', env('SMTP_FROM_EMAIL'));
-  define('SMTP_FROM_NAME', env('SMTP_FROM_NAME'));
-*/
 
 class SMTPMail {
 
